80caae9c23b29c3b4a5eb7e6db95758aa6e1c6cf
josers77j <josers77.1@gmail.com>
2023-08-19 15:28:10 -0600

    Ejercicio terminado

--- FormsPractica2/Form1.Designer.cs
+++ FormsPractica2/Form1.Designer.cs
@@ -28,12 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Porcentajetextbox = new System.Windows.Forms.TextBox();
+            this.MontoInicialtextbox = new System.Windows.Forms.TextBox();
+            this.Calcularbtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Resultadolbl = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Porcentajetextbox
+            // 
+            this.Porcentajetextbox.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Porcentajetextbox.Location = new System.Drawing.Point(21, 188);
+            this.Porcentajetextbox.Name = "Porcentajetextbox";
+            this.Porcentajetextbox.Size = new System.Drawing.Size(199, 36);
+            this.Porcentajetextbox.TabIndex = 3;
+            // 
+            // MontoInicialtextbox
+            // 
+            this.MontoInicialtextbox.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MontoInicialtextbox.Location = new System.Drawing.Point(15, 79);
+            this.MontoInicialtextbox.Name = "MontoInicialtextbox";
+            this.MontoInicialtextbox.Size = new System.Drawing.Size(199, 36);
+            this.MontoInicialtextbox.TabIndex = 4;
+            // 
+            // Calcularbtn
+            // 
+            this.Calcularbtn.Font = new System.Drawing.Font("Poppins Medium", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Calcularbtn.Location = new System.Drawing.Point(624, 211);
+            this.Calcularbtn.Name = "Calcularbtn";
+            this.Calcularbtn.Size = new System.Drawing.Size(140, 40);
+            this.Calcularbtn.TabIndex = 2;
+            this.Calcularbtn.Text = "Calcular";
+            this.Calcularbtn.UseVisualStyleBackColor = true;
+            this.Calcularbtn.Click += new System.EventHandler(this.Calcularbtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(15, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 34);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Monto Inicial :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(15, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 34);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Porcentaje :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Poppins", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 62);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Interes Compuesto";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.groupBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox2.Controls.Add(this.Resultadolbl);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Inch);
+            this.groupBox2.Location = new System.Drawing.Point(12, 337);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(776, 101);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // Resultadolbl
+            // 
+            this.Resultadolbl.AutoSize = true;
+            this.Resultadolbl.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.Resultadolbl.ForeColor = System.Drawing.Color.White;
+            this.Resultadolbl.Location = new System.Drawing.Point(148, 36);
+            this.Resultadolbl.Name = "Resultadolbl";
+            this.Resultadolbl.Size = new System.Drawing.Size(27, 34);
+            this.Resultadolbl.TabIndex = 5;
+            this.Resultadolbl.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(21, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 34);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Resultado :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.Calcularbtn);
+            this.groupBox1.Controls.Add(this.Porcentajetextbox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.MontoInicialtextbox);
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(12, 74);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox1.Size = new System.Drawing.Size(776, 257);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 453);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private Label label1;
+        private GroupBox groupBox2;
+        private Button Calcularbtn;
+        private Label label2;
+        private Label label3;
+        private Label Resultadolbl;
+        private Label label4;
+        private TextBox Porcentajetextbox;
+        private TextBox MontoInicialtextbox;
+        private GroupBox groupBox1;
     }
 }
--- FormsPractica2/Form1.cs
+++ FormsPractica2/Form1.cs
@@ -6,5 +6,22 @@ namespace FormsPractica2
         {
             InitializeComponent();
         }
+
+        private void Calcularbtn_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(MontoInicialtextbox.Text) &&
+                !string.IsNullOrEmpty(Porcentajetextbox.Text) && 
+                float.TryParse(Porcentajetextbox.Text, out float porcentaje) &&
+                float.TryParse(MontoInicialtextbox.Text, out float montoinicial)
+                )
+            {
+                var res = montoinicial + (montoinicial * (porcentaje /100));
+                Resultadolbl.Text = $"{res}";
+            }
+            else
+            {
+                MessageBox.Show("PRO PRO PRO");
+            }
+        }
     }
 }
